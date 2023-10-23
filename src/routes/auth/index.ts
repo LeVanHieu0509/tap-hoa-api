6ab505f399725f735387c5a57ffaa7fbf3dcff1e
LeVanHieu0509 <leVanHieu.huflit@gmail.com>
2023-10-23 16:26:04 +0700

    add celebrate

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,8 +1,10 @@
 import AuthController from "../../apps/controller/auth.controller";
 import { asyncHandler } from "../../helpers/asyncHandler";
+const { celebrate } = require("celebrate");
+const dto = require("../../apps/dto/signin.dto");
 
 const router = require("express").Router();
 
-router.post("/user/signup", asyncHandler(AuthController.signUp));
+router.post("/user/signup", celebrate(dto.signUpDto), asyncHandler(AuthController.signUp));
 
 export default router;
